6f3c150d1708630fce49fd0ffdae7875b9353d66
shoxrux002 <turdimoshoxrux@gmail.com>
2022-05-16 17:06:21 +0500

    migrations

--- .env/bin/activate.fish
+++ .env/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/user/my folde/findjob/.env"
+set -gx VIRTUAL_ENV "/home/shohrux/projects/findjob/.env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
