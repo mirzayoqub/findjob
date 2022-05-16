8be628ee0f7b46da366597cf28c9f572519a545a
yoqub <darkdarkstalker250@gmail.com>
2022-05-16 16:25:00 +0500

    findjob1

--- account/models.py
+++ account/models.py
@@ -1,22 +1,41 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-
-
-class Job(models.Model):
-    name = models.CharField(max_length=20)
-    img = models.ImageField()
-
-
-class Type(models.Model):
-    job = models.ForeignKey(Job, on_delete=models.RESTRICT)
-    name = models.CharField(max_length=20)
-    img = models.ImageField()
+from main.models import Type
 
 
 class User(AbstractUser):
-    type = models.ForeignKey(Type, on_delete=models.RESTRICT)
+    pass
+
+
+class TypeUser(models.Model):
+    name = models.CharField(max_length=20)
+
+
+class Employee(models.Model):
+    user = models.ForeignKey(User, on_delete=models.RESTRICT)
+    experiance = models.CharField(max_length=50)
+    type_user = models.ForeignKey(TypeUser, on_delete=models.CASCADE)
+    type = models.ManyToManyField(Type)
     price = models.IntegerField()
+    old = models.IntegerField()
     img = models.ImageField(upload_to='uploads')
     comment = models.TextField()
-    date_post = models.DateField()
+    date_post = models.DateField(auto_now_add=True)
+    phone = models.CharField(max_length=50)
+    address = models.CharField(max_length=100)
+    language = models.CharField(max_length=50)
+    contact = models.URLField()
 
+
+class Employer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.RESTRICT)
+    skills = models.CharField(max_length=100)
+    type = models.ForeignKey(Type, on_delete=models.RESTRICT)
+    img = models.ImageField(upload_to='uploads')
+    comment = models.TextField()
+    language = models.CharField(max_length=50)
+    date_post = models.DateField()
+    price = models.IntegerField()
+    phone = models.CharField(max_length=50)
+    address = models.CharField(max_length=100)
+    contact = models.URLField()
--- main/models.py
+++ main/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Job(models.Model):
+    name = models.CharField(max_length=20)
+    img = models.ImageField()
+
+
+class Type(models.Model):
+    job = models.ForeignKey(Job, on_delete=models.RESTRICT)
+    name = models.CharField(max_length=20)
+    img = models.ImageField()
+
+
+class Skills(models.Model):
+    name = models.CharField(max_length=20)
+    type = models.ForeignKey(Type, on_delete=models.CASCADE)
