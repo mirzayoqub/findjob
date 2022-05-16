08f318172b2b7197ad166fca9ac4b10d43a61b5b
yoqub <darkdarkstalker250@gmail.com>
2022-05-16 14:29:07 +0500

    findjob

--- findjob/settings.py
+++ findjob/settings.py
@@ -11,21 +11,27 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import environ
+import os
 
+env = environ.Env(
+    DEBUG=(bool, False)
+)
+
+environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bx*83=a+8-@^mg&zlzx#p2kn0jg%mr*@1_ht4l99#dz#jatp@0'
 
+SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env('ALLOWED_HOSTS', cast=list)
 
 
 # Application definition
@@ -76,12 +82,7 @@ WSGI_APPLICATION = 'findjob.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'findjob',
-        'USER': 'root',
-        'PASSWORD': 'Qwerty123$'
-    }
+    'default': env.db()
 }
 
 
